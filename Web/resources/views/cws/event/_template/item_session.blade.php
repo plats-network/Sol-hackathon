ebca423f5bf969ebe3f51426e3f730bcd7edbcc0
vaix-dungpa <dungpa@vaixgroup.com>
2024-04-04 00:54:01 +0700

    add mint booth, session

--- Web/resources/views/cws/event/_template/item_session.blade.php
+++ Web/resources/views/cws/event/_template/item_session.blade.php
@@ -12,6 +12,17 @@
                 @endforeach
             </select>
         </div>
+        <div class="col-lg-4 d-flex align-items-end mb-2">
+            <button
+                type="button"
+                class="btn btn-primary w-sm ms-auto connect_wallet">Connect Wallet
+            </button>
+            <button
+                type="button"
+                class="btn btn-primary w-sm ms-auto mint_nft" value="1" style="display: none">
+                Mint NFT
+            </button>
+        </div>
     </div>
     <div class="col-sm-4">
         <input
