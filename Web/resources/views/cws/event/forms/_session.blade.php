989ce8c0c842c78ee8fef476e752c29fb424f260
Phi Tuan <phituan@Phis-MacBook-Pro.local>
2024-04-04 02:10:18 +0700

    update btn

--- Web/resources/views/cws/event/forms/_session.blade.php
+++ Web/resources/views/cws/event/forms/_session.blade.php
@@ -197,6 +197,9 @@
                                         Mint NFT
                                     </button>
                                 </div>
+                                <div class="mb-3 col-5">
+                                    <a href="#" style="display: none" id="mint-sol" class="link-primary">Solana Explorer</a>
+                                </div>
                             </div>
                             <div class="col-sm-4">
                                 <label class="col-form-label">Name <span class="text-danger" style="font-size: 11px;">(*)</span></label>
